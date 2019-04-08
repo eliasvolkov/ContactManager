b28681adfe7b8ae531d0286f9b1da0dd040fc8b9
Ilia Volkov <ilya.j0b@yandex.ru>
2019-04-08 17:41:20 +0300

    add Provider

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.css";
 import Header from "./components/Header.js";
 import ContactList from "./components/ContactList";
+import { Provider } from "./context";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header brand="Contact manager" />
-        <div className="container">
-          <ContactList />
+      <Provider>
+        <div>
+          <Header brand="Contact manager" />
+          <div className="container">
+            <ContactList />
+          </div>
         </div>
-      </div>
+      </Provider>
     );
   }
 }
